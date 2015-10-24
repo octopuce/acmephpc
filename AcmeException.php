b9d9d208f10c41f73ecc55c8e649c13fdb3a9d0b
Benjamin Sonntag <benjamin@octopuce.fr>
2015-10-24 18:12:32 +0200

    adding simple Exception class

---
+++ AcmeException.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the ACME PHP Client Library
+ * (C) 2015 Benjamin Sonntag <benjamin@octopuce.fr>
+ * distributed under LPGL 2.1+ see LICENSE file
+ */
+
+namespace Octopuce\Acme;
+
+/**
+ * AcmeException are exception thrown by the AcmePhpClient library
+ * they contain a code (unique number) and a message (english string)
+ * @author benjamin
+ */
+class AcmeException extends Exception {
+
+    public $code = 0;
+    public $messsage = "";
+
+    function __construct($code, $message) {
+        $this->code = $code;
+        $this->message = $message;
+    }
+
+}
+
