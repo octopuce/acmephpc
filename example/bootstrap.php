239b26726776fca55ceefa9863a4a2764804891f
Mathieu Duplouy <mathieu.duplouy@gmail.com>
2016-01-29 17:01:59 +0100

    Moved config to file

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -2,31 +2,7 @@
 
 $autoload = require __DIR__.'/../vendor/autoload.php';
 
-/**
- * Define configuration or override services here
- */
-$config = array(
-    'params' => array(
-        'api' => 'https://acme-staging.api.letsencrypt.org',
-        'storage' => array(
-            // The storage type to use
-            'type' => 'filesystem',
-            // Storage config
-            'filesystem' => __DIR__.'/../storage',
-            'database' => array('dsn' => 'mysql://letsencrypt:le2015@localhost/letsencrypt'),
-        ),
-        'challenge' => array(
-            'type' => 'http',
-            'config' => array(
-                // The target to store the file
-                'target-path' => '/tmp/',
-            ),
-        ),
-        // Default account to be used
-        'account' => 'me@you.com',
-    ),
-);
-
+$config = require __DIR__.'/../src/config.php';
 
 // Get client
 $client = new \Octopuce\Acme\Client($config);
